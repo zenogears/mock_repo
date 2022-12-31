6e23dd4d213b73dcffb3305f45087b05a93edc1a
Chibiko <zenogears.aka@gmail.com>
2023-01-01 01:57:35 +0300

    add code in files types: .cpp remove code in files types: .cpp

---
+++ content.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+        cout << "lkftk";
+        cout << "imdfi";
+        cout << "qcwng";
+        cout << "sdjrh";
+        cout << "mfcdm";
+return 0;
+}
