b0bf43a3da97c960b2e96044019409ccd8a5e3d5
Chibiko <zenogears.aka@gmail.com>
2023-01-03 09:53:19 +0300

    add code in files types: .cpp,.h remove code in files types: .cpp

--- content.cpp
+++ content.cpp
@@ -2,10 +2,28 @@
 using namespace std;
 
 int main() {
-        cout << "kfaco";
-        cout << "qjfgl";
-        cout << "nhiol";
-        cout << "oflxp";
-        cout << "lxuiq";
+        cout << "ymekn";
+        cout << "vnpim";
+        cout << "vkrim";
+        cout << "yhqjt";
+        cout << "qjhrm";
+        cout << "ilchq";
+        cout << "hvicc";
+        cout << "spjrx";
+        cout << "mbryf";
+        cout << "jdbrc";
+        cout << "nafeq";
+        cout << "cojtj";
+        cout << "icgyh";
+        cout << "iqrrf";
+        cout << "ssypj";
+        cout << "jvtmu";
+        cout << "nxipi";
+        cout << "tjagf";
+        cout << "nggne";
+        cout << "pclbo";
+        cout << "pqwbp";
+        cout << "vwchb";
+        cout << "jiwno";
 return 0;
 }
